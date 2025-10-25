be7e572b9133d594a1868a4f2741425f80fe73a2
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2025-10-25 04:59:21 +0200

    Fix typo in file name

---
+++ ch02/ex01/ch02-ex01.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#define ANSWER 42  // Named constant instead of magic number
+
+int main(void)
+{ int unused = ANSWER;  // clang-tidy should warn: unused variable 'unused'
+    printf("hello, world\n");
+}

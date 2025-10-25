c2ffb62a3ecc0196ca0cd0f9bd9f4462f402b395
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2025-10-25 13:07:40 +0200

    More tests with Clang-Tidy.

--- ch02/ex01/ch02-ex01.c
+++ ch02/ex01/ch02-ex01.c
@@ -5,6 +5,5 @@
 int main(void)
 { int unused = ANSWER;  // clang-tidy should warn: unused variable 'unused'
     printf("hello, world\n");
-    int x = 1;
-
+    int x = 1; // Another unused variable that should give a warning
 }

9c5840e70ac7bcdabb463528eecd9c7ed7c63c8f
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2025-10-28 03:26:56 +0100

    Create programming project 2.1, King (2008)

---
+++ ch02/pr01/ch02-pr01.c
@@ -0,0 +1,11 @@
+/* Program that uses printf to display a picture of a check mark. */
+#include <stdio.h>
+
+int main(void) {
+  printf("       *\n");
+  printf("      * \n");
+  printf("     *  \n");
+  printf("*   *   \n");
+  printf(" * *    \n");
+  printf("  *     \n");
+}

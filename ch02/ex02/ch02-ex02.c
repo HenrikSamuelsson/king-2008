38d965683ed5aee0e320342c07937c78c4b4db13
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2025-10-29 04:46:48 +0100

    Add code part of exercise 2.2

---
+++ ch02/ex02/ch02-ex02.c
@@ -0,0 +1,14 @@
+/* 
+ * Program from King, K. N. (2008). C Programming: A Modern Approach (2nd ed.).
+ * Chapter 2, Exercise 2.
+ *
+ * Prints Parkinson's law using printf().
+ */
+#include <stdio.h>
+
+int main(void) {
+  printf("Parkinson's Law:\nWork expands so as to ");
+  printf("fill the time\n");
+  printf("available for its completion.\n");
+  return 0;
+}

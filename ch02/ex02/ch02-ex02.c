9c7ed698bafb1445729e8c48f6cba7593de9d437
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2025-10-30 03:42:00 +0100

    Format using four spaces for indentation.

--- ch02/ex02/ch02-ex02.c
+++ ch02/ex02/ch02-ex02.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * Program from King, K. N. (2008). C Programming: A Modern Approach (2nd ed.).
  * Chapter 2, Exercise 2.
  *
@@ -7,8 +7,8 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("Parkinson's Law:\nWork expands so as to ");
-  printf("fill the time\n");
-  printf("available for its completion.\n");
-  return 0;
+    printf("Parkinson's Law:\nWork expands so as to ");
+    printf("fill the time\n");
+    printf("available for its completion.\n");
+    return 0;
 }

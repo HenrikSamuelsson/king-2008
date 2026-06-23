bc68ea146e3d6685fc7f902b1714ab8d0dc932bd
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2026-06-23 05:58:40 +0200

    Add description for new project.

---
+++ ch02/project_04/king2008_ch02_pr04.c
@@ -0,0 +1,7 @@
+/*
+ * Solution to Programming Project 2 in Chapter 4 of:
+ *
+ * King, K. N. (2008). C Programming: A Modern Approach (2nd ed.).
+ *
+ * Calculates and displays an amount with tax based on a user entered amount.
+ */

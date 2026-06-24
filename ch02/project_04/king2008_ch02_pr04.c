53d7d1796338c880caa21013160a903b71779076
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2026-06-24 19:04:31 +0200

    Add solution to problem 2.4

--- ch02/project_04/king2008_ch02_pr04.c
+++ ch02/project_04/king2008_ch02_pr04.c
@@ -3,5 +3,18 @@
  *
  * King, K. N. (2008). C Programming: A Modern Approach (2nd ed.).
  *
- * Calculates and displays an amount with tax based on a user entered amount.
+ * Takes an amount and calculates the amount with 5 percent tax added.
  */
+#include <stdio.h>
+
+int main(void) {
+    printf("Enter an amount: ");
+
+    float amount;
+    scanf("%f", &amount);
+
+    float tax = 0.05 * amount;
+    printf("With tax added: %.2f\n", amount + tax);
+
+    return 0;
+}
